4637ba2d9f02455042c8552b2f955c3129e1cf22
Sara Ibarra <saraiba33@gmail.com>
2022-05-25 16:27:38 -0600

    Add solution

--- App.java
+++ App.java
@@ -5,17 +5,20 @@ class App {
     int[] testScores = { 72, 95, 91, 85, 87, 51, 65, 72, 75, 89 };
     int length = testScores.length;
 
-    System.out.println(mean(testScores, length));
     System.out.println(median(testScores, length));
+    System.out.println(mean(testScores, length));
+    System.out.println(highScore(testScores));
+    System.out.println(lowScore(testScores));
+
   }
 
   public static double median(int[] array, int length) {
     Arrays.sort(array);
-    if (length % 2 != 0)
+    if (length % 2 != 0) {
       return (double) array[length / 2];
-
-    return (double) (array[(length - 1) - 1] + array[length / 2]) / 2.0;
-
+    } else {
+      return ((double) array[(length / 2)] + (double) array[length / 2 - 1]) / 2;
+    }
   }
 
   public static double mean(int[] array, int length) {
@@ -25,5 +28,15 @@ class App {
       score++;
     }
     return (sum / length);
+  }
+
+  public static int highScore(int[] array) {
+    Arrays.sort(array);
+    return array[array.length - 1];
+  }
+
+  public static int lowScore(int[] array) {
+    Arrays.sort(array);
+    return array[0];
   }
 }
